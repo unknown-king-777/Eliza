a917903e3e00d389251469a68ace6652742fdff4
smfahimcp <158431295+smfahimcp@users.noreply.github.com>
2024-03-19 19:34:50 +0600

    Update groupconfig.js

--- scripts/cmds/groupconfig.js
+++ scripts/cmds/groupconfig.js
@@ -12,7 +12,7 @@ module.exports = {
 			vi: "Bật tắt chức năng chống thành viên đổi thông tin box chat của bạn",
 			en: "Turn on/off anti change info box"
 		},
-		category: "box chat",
+		category: "config",
 		guide: {
 			vi: "   {pn} avt [on | off]: chống đổi avatar box chat"
 				+ "\n   {pn} name [on | off]: chống đổi tên box chat"

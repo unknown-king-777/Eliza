b2256aac458ae0179097a69e3897f0231c0aee44
smfahimcp <158431295+smfahimcp@users.noreply.github.com>
2024-03-26 05:59:00 +0600

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -38,14 +38,15 @@ module.exports = {
     if (ping > 1500) {
       pingStatus = "🔴 | Very Bad";
     }
-    const uptime = process.uptime();
-    const days = Math.floor(uptime / 1000 % 3600 % 24);
-    const hours = Math.floor(uptime / 3600);
-    const minutes = Math.floor((uptime % 3600) / 60);
-    const seconds = Math.floor(uptime % 60);
+    const uptime = {
+        days: Math.floor(now.getTime() / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((now.getTime() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        minutes: Math.floor((now.getTime() % (1000 * 60 * 60)) / (1000 * 60)),
+        seconds: Math.floor((now.getTime() % (1000 * 60)) / 1000),
+      };
     let currentDate = moment().tz('Asia/Dhaka').format('YYYY-MM-DD hh:mm:ss A'); // Format in 12-hour with AM/PM
-    const uptimeString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
+    const uptimeString = `${uptime.days}d ${uptime.hours}h ${uptime.minutes}m ${uptime.seconds}s`;
+    
     await api.sendMessage(`Group Test Bot Current Speed: ${ping} ms.\nSpeed Status: ${pingStatus}\n\nUptime: ${uptimeString}\nDate: ${currentDate}`, event.threadID);
   }
 };

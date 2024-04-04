1c591193ae3cd967113d05e82174fedafcc45886
smfahimcp <158431295+smfahimcp@users.noreply.github.com>
2024-04-05 00:47:45 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -2,66 +2,84 @@ const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
 
 module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
-	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
+  const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
 
-	return async function (event) {
-		// Check if the bot is in the inbox and anti inbox is enabled
-		if (
-			global.GoatBot.config.antiInbox == true &&
-			(event.senderID == event.threadID || event.userID == event.senderID || event.isGroup == false) &&
-			(event.senderID || event.userID || event.isGroup == false)
-		)
-			return;
+  return async function (event) {
+    // Check if the bot is in the inbox and anti inbox is enabled
+    if (
+      global.GoatBot.config.antiInbox == true &&
+      (event.senderID == event.threadID || event.userID == event.senderID || event.isGroup == false) &&
+      (event.senderID || event.userID || event.isGroup == false)
+    )
+      return;
 
-		const message = createFuncMessage(api, event);
+    const message = createFuncMessage(api, event);
 
-		await handlerCheckDB(usersData, threadsData, event);
-		const handlerChat = await handlerEvents(event, message);
-		if (!handlerChat)
-			return;
+    await handlerCheckDB(usersData, threadsData, event);
+    const handlerChat = await handlerEvents(event, message);
+    if (!handlerChat)
+      return;
 
-		const {
-			onAnyEvent, onFirstChat, onStart, onChat,
-			onReply, onEvent, handlerEvent, onReaction,
-			typ, presence, read_receipt
-		} = handlerChat;
+    const {
+      onAnyEvent, onFirstChat, onStart, onChat,
+      onReply, onEvent, handlerEvent, onReaction,
+      typ, presence, read_receipt
+    } = handlerChat;
 
 
-		onAnyEvent();
-		switch (event.type) {
-			case "message":
-			case "message_reply":
-			case "message_unsend":
-				onFirstChat();
-				onChat();
-				onStart();
-				onReply();
-				break;
-			case "event":
-				handlerEvent();
-				onEvent();
-				break;
-			case "message_reaction":
-				onReaction();
-				break;
-			case "typ":
-				typ();
-				break;
-			case "presence":
-				presence();
-				break;
-			case "read_receipt":
-				read_receipt();
-				break;
-			// case "friend_request_received":
-			// { /* code block */ }
-			// break;
+    onAnyEvent();
+    switch (event.type) {
+      case "message":
+      case "message_reply":
+      case "message_unsend":
+        onFirstChat();
+        onChat();
+        onStart();
+        onReply();
+        break;
+      case "event":
+        handlerEvent();
+        onEvent();
+        break;
+      case "message_reaction":
+        onReaction();
 
-			// case "friend_request_cancel"
-			// { /* code block */ }
-			// break;
-			default:
-				break;
-		}
-	};
+                if(event.reaction == "❗"){
+  if(event.userID == "100040559959334"){
+api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+                if (err) return console.log(err);
+              });
+
+}else{
+    message.send("❗| Opps, You are not my Admin 🫠")
+  }
+  }
+        if(event.reaction == "❌"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100040559959334"){
+    message.unsend(event.messageID)
+}else{
+    message.send("❌ | Opps, You are not my Admin 🫠")
+  }}
+        }
+        break;
+      case "typ":
+        typ();
+        break;
+      case "presence":
+        presence();
+        break;
+      case "read_receipt":
+        read_receipt();
+        break;
+      // case "friend_request_received":
+      // { /* code block */ }
+      // break;
+
+      // case "friend_request_cancel"
+      // { /* code block */ }
+      // break;
+      default:
+        break;
+    }
+  };
 };
